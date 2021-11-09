4fef02753c1fd6422d0521b24ffcee125ca6ad19
koffela <46707662+koffela@users.noreply.github.com>
2021-11-09 15:42:46 -0800

    Added a gradient to the suspicion meter
    Gradient changes from green to yellow to red as the suspicion accumulates in the player's sus meter.

--- Bug/Assets/UI/SuspicionBar.cs
+++ Bug/Assets/UI/SuspicionBar.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class SuspicionBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     
     public void SetMinSuspicion(int suspicion)
 	{
         slider.minValue = suspicion;
         slider.value = suspicion;
+
+        fill.color = gradient.Evaluate(0f);
 	}
 
     public void SetSuspicion(int suspicion)
 	{
         slider.value = suspicion;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
 	}
 
 }
