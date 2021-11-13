4543142ce90b88313e6db051eb28f8a0dee25135
David Wright <dwright.bz@gmail.com>
2021-11-13 15:16:06 -0800

    Preliminary collider/movement WIP
    VERY preliminary work on 2D collisions, managing map movement, camera host following/zoom option

---
+++ Bug/Assets/Framework/Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace Bug
+{
+    public class Singleton<T> where T : Singleton<T>, new()
+    {
+        protected Singleton()
+        {
+        }
+
+        public static T Instance { get { return Nested.instance; } }
+
+        private class Nested
+        {
+            static Nested() 
+            { 
+            }
+
+            internal static readonly T instance = new T();
+        }
+    }
+}
