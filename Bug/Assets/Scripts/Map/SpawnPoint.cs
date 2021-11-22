552f9b2b0bcbac7e93cbef7b4ced9988400e3f05
Noah <35441938+noahfarber@users.noreply.github.com>
2021-11-21 19:22:44 -0800

    More map updates. Places doors. Adds beginning of spawner creation.

---
+++ Bug/Assets/Scripts/Map/SpawnPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public HostCharactersType RegisteredCharacters = HostCharactersType.Engineer;
+    public int SecurityLevelMin;
+    public int SecurityLevelMax;
+}
