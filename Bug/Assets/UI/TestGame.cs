74c09f9528fa5437ed086c808c973e21a264a4e6
koffela <46707662+koffela@users.noreply.github.com>
2021-11-29 20:28:50 -0800

    Improvements to matching game
    Added a parameter for number of cards, and number of allowed moves. Added a background for both the base game scene and the puzzle table. Also ensured that a new instance of the game can be destroyed and created each time a new game is called.

---
+++ Bug/Assets/UI/TestGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGame : MonoBehaviour
+{
+    public AddButtonsScript addButtonsScript;
+    public GameController gameController;
+    // Start is called before the first frame update
+
+    public static bool cardgameActive = false;
+    public GameObject puzzleField;
+    public GameObject puzzleExitButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+		{
+            //Time.timeScale = 0f;
+            puzzleField.SetActive(true);
+            puzzleExitButton.SetActive(true);
+            addButtonsScript.GameSetup(8);
+            gameController.allowedNumberOfMoves = 5;
+            gameController.BeginGame();
+		}
+    }
+    
+    
+}
